603d501c1a8e4aee3807693e9ecd1c8e107bc984
weiwei <weiwei@tenxcloud.com>
2020-11-03 11:27:15 +0800

    patch

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir -p data/{confluence,mysql,conf.d}
-chmod +x 777 data/confluence
-cp patch/mysql.cnf  data/conf.d
+chmod 777 data/confluence
+cp -f patch/mysql.cnf  data/conf.d
 
-docker-compose -f docker-compose-mysql.yaml up -d
+docker-compose -f docker-compose-mysql.yml up -d
